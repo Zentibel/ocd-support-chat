b0ec3768562a585b013d377ad01b565666f88790
Evan Coury <me@evancoury.com>
2017-01-15 12:01:49 -0700

    Drop version number down to 0

--- src/Common/Helper/Git.php
+++ src/Common/Helper/Git.php
@@ -8,8 +8,8 @@ class Git
         exec('git describe --always',$version_mini_hash);
         exec('git rev-list HEAD | wc -l',$version_number);
         exec('git log -1',$line);
-        $version['short'] = "v1.".trim($version_number[0]).".".$version_mini_hash[0];
-        $version['full'] = "v1.".trim($version_number[0]).".$version_mini_hash[0] (".str_replace('commit ','',$line[0]).")";
+        $version['short'] = "v0.".trim($version_number[0]).".".$version_mini_hash[0];
+        $version['full'] = "v0.".trim($version_number[0]).".$version_mini_hash[0] (".str_replace('commit ','',$line[0]).")";
         return $version;
     }
 }
