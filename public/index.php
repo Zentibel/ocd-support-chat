3feb396a392f53db15c5c18f34467ed13bf9163c
Evan Coury <me@evancoury.com>
2017-04-22 16:23:21 -0700

    Make usernames a proper link and open in a new tab

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
+if (php_sapi_name() === 'cli-server'
+    && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
+) {
+    return false;
+}
+
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 $container = require 'config/container.php';
-
-//$handler = new Predis\Session\Handler(
-//    $container->get('redis'),
-//    ['gc_maxlifetime' => 86400 * 180]
-//);
-//session_set_save_handler($handler);
 
 $app = $container->get(Zend\Expressive\Application::class);
 
