abd31682ab79ee2c2b1fd389a00bec1f9f7c7d18
Evan Coury <me@evancoury.com>
2017-05-29 11:41:44 -0700

    Really fix shruggy face

--- src/Chat/Listener/MessageProjector.php
+++ src/Chat/Listener/MessageProjector.php
@@ -40,7 +40,7 @@ class MessageProjector
             'id'        => $e->messageId,
             'sender'    => $e->userId,
             'roomId'    => $e->roomId,
-            'message'   => str_replace('¯\_(ツ)_/¯', '¯\\\_(ツ)_/¯', $e->message),
+            'message'   => str_replace('¯\_(ツ)_/¯', '¯\\\_(ツ)\_/¯', $e->message),
             'timestamp' => $now,
         ];
 
