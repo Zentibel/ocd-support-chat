a36411df42427a6c64bf4bad17124f29feb0ca6b
Evan Coury <me@evancoury.com>
2017-05-30 22:47:30 -0700

    Re-disable registration

--- config/autoload/routes.auth.global.php
+++ config/autoload/routes.auth.global.php
@@ -15,47 +15,47 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'auth-register',
-            'path' => '/register',
-            'middleware' => [
-                Common\Middleware\ValidateCsrf::class,
-                //Auth\Middleware\VerifyCaptcha::class,
-                Common\Middleware\CqrsPayloadFromPost::class,
-                Common\Middleware\ValidateCqrsPayload::class,
-                Common\Middleware\GenerateNewUuid::class,
-                Auth\Middleware\HashPasswordInput::class,
-                Common\Middleware\DispatchCqrsRequest::class,
-                Auth\Middleware\Authenticate::class,
-                Common\Middleware\Redirect::class,
-            ],
-            'allowed_methods' => ['POST'],
-            'options' => [
-                'defaults' => [
-                    'input_validator' => Auth\Validator\RegisterUser::class,
-                    'cqrs_target' => Auth\Command\RegisterUser::class,
-                    'new_uuid_field' => 'userId',
-                    'redirect_route'  => 'community-chat',
-                    'redirect_params' => 'community:ocd',
-                    'cqrs_ignore_fields' => 'password',
-                ],
-            ],
-        ],
-        [
-            'name' => 'auth-username-available',
-            'path' => '/register/check',
-            'middleware' => [
-                Common\Middleware\CqrsPayloadFromQuery::class,
-                Common\Middleware\DispatchCqrsRequest::class,
-                Common\Middleware\ReturnJsonResponse::class,
-            ],
-            'allowed_methods' => ['GET'],
-            'options' => [
-                'defaults' => [
-                    'cqrs_target' => Auth\Query\UsernameAvailable::class
-                ],
-            ],
-        ],
+        //[
+        //    'name' => 'auth-register',
+        //    'path' => '/register',
+        //    'middleware' => [
+        //        Common\Middleware\ValidateCsrf::class,
+        //        //Auth\Middleware\VerifyCaptcha::class,
+        //        Common\Middleware\CqrsPayloadFromPost::class,
+        //        Common\Middleware\ValidateCqrsPayload::class,
+        //        Common\Middleware\GenerateNewUuid::class,
+        //        Auth\Middleware\HashPasswordInput::class,
+        //        Common\Middleware\DispatchCqrsRequest::class,
+        //        Auth\Middleware\Authenticate::class,
+        //        Common\Middleware\Redirect::class,
+        //    ],
+        //    'allowed_methods' => ['POST'],
+        //    'options' => [
+        //        'defaults' => [
+        //            'input_validator' => Auth\Validator\RegisterUser::class,
+        //            'cqrs_target' => Auth\Command\RegisterUser::class,
+        //            'new_uuid_field' => 'userId',
+        //            'redirect_route'  => 'community-chat',
+        //            'redirect_params' => 'community:ocd',
+        //            'cqrs_ignore_fields' => 'password',
+        //        ],
+        //    ],
+        //],
+        //[
+        //    'name' => 'auth-username-available',
+        //    'path' => '/register/check',
+        //    'middleware' => [
+        //        Common\Middleware\CqrsPayloadFromQuery::class,
+        //        Common\Middleware\DispatchCqrsRequest::class,
+        //        Common\Middleware\ReturnJsonResponse::class,
+        //    ],
+        //    'allowed_methods' => ['GET'],
+        //    'options' => [
+        //        'defaults' => [
+        //            'cqrs_target' => Auth\Query\UsernameAvailable::class
+        //        ],
+        //    ],
+        //],
         [
             'name' => 'auth-login-form',
             'path' => '/login',
