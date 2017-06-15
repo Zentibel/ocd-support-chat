f3dbc194bdd4474448700216b19ae7ac0e31a303
Evan Coury <me@evancoury.com>
2017-06-14 20:32:20 -0700

    Added voice chat and made avatars into PM links

---
+++ public/voice-chat.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var inVoiceChat = false;
+    $('#voice-chat-button').click(function(event) {
+        event.preventDefault();
+        if (!inVoiceChat) {
+            $('body').append('<iframe id="voice-chat-frame" src="https://www.talk.gg/ochatd" style="width: 1px; height: 1px;"></iframe>');
+            $('#voice-chat-frame').hide();
+            $('#voice-chat-button').text('Leave Voice Chat');
+            inVoiceChat = true;
+            showToast('You are now in the voice chat.');
+        } else {
+            $('#voice-chat-frame').remove();
+            $('#voice-chat-button').text('Join Voice Chat');
+            inVoiceChat = false;
+            showToast('You have left the voice chat.');
+        }
+    });
+});
